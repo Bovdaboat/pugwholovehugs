c3a5b707c8fb3e763ad4b2974f9719a4f8207a60
bovdaboat <ignatgabov@gmail.com>
2023-06-11 17:33:05 -0700

    tailwind and finisheding the nav bar sectiom

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,14 @@
 import Image from 'next/image'
 import Navbar from "@/app/shared/Navbar";
+import Hero from "@/app/shared/Hero";
 
 export default function Home() {
   return (
       <>
       <section>
           <Navbar />
-        <div>Hero</div>
+            <Hero />
+          <div>Pic of the two pugs</div>
       </section>
       </>
   )
---
+++ src/app/shared/Hero.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Hero = () => {
+    return(
+        <div>
+            <h1 className="text-2xl flex justify-center">About us</h1>
+            <p></p>
+        </div>
+    )
+}
+
+
+export default Hero
