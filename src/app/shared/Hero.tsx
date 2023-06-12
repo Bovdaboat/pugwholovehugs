4acc43e20c51cb4352bdd2d7ce176644364e401b
bovdaboat <ignatgabov@gmail.com>
2023-06-11 17:40:47 -0700

    did the hero section for the pugs

--- src/app/shared/Hero.tsx
+++ src/app/shared/Hero.tsx
@@ -3,8 +3,14 @@ import React from "react";
 const Hero = () => {
     return(
         <div>
-            <h1 className="text-2xl flex justify-center">About us</h1>
-            <p></p>
+            <h1 className="text-2xl flex justify-center my-5 mx-5">About us</h1>
+            <p>Once upon a time, in a cozy little house, lived two adorable pug sisters named TsuNami and Stella.
+                These two little furballs were inseparable. From the moment they were born, they formed an unbreakable
+                bond that only grew stronger with time. Stella, the older sister, was a playful and adventurous pug with
+                a mischievous twinkle in her eyes. TsuNami, the younger one, was a gentle and affectionate soul, always
+                seeking comfort in her sister's presence. They spent their days frolicking in the backyard, chasing
+                each other's tails, and exploring the world together.
+            </p>
         </div>
     )
 }
