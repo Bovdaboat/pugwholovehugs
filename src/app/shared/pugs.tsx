8b8b626e00e9ef6d0b4a4e1f174512793ba96e07
bovdaboat <ignatgabov@gmail.com>
2023-06-12 22:11:52 -0700

    tailwind config

---
+++ src/app/shared/pugs.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+
+const Pugs = () => {
+    return(
+        <>
+            <div className="grid-cols-2">
+        <div>This is going be TsuNami</div>
+        <div>This is going be stella</div>
+            </div>
+            </>
+    )
+}
+
+export default Pugs
