3c7492035cd5484689bd3cdb763f3253b7da07eb
bovdaboat <ignatgabov@gmail.com>
2023-06-12 22:25:56 -0700

    added the two slots just need to add the pics and use the svg

--- src/app/shared/Hero.tsx
+++ src/app/shared/Hero.tsx
@@ -3,7 +3,8 @@ import React from "react";
 const Hero = () => {
     return(
         <div>
-            <h1 className="text-2xl flex justify-center my-5 mx-5">About us</h1>
+            <h1 className="text-2xl flex justify-center m-3 p-3">About us</h1>
+            <div className="m-3 p-4">
             <p>Once upon a time, in a cozy little house, lived two adorable pug sisters named TsuNami and Stella.
                 These two little furballs were inseparable. From the moment they were born, they formed an unbreakable
                 bond that only grew stronger with time. Stella, the older sister, was a playful and adventurous pug with
@@ -11,6 +12,7 @@ const Hero = () => {
                 seeking comfort in her sister's presence. They spent their days frolicking in the backyard, chasing
                 each other's tails, and exploring the world together.
             </p>
+            </div>
         </div>
     )
 }
--- src/app/shared/pugs.tsx
+++ src/app/shared/pugs.tsx
@@ -4,11 +4,12 @@ import React from "react";
 const Pugs = () => {
     return(
         <>
-            <div className="grid-cols-2">
-        <div>This is going be TsuNami</div>
-        <div>This is going be stella</div>
+            <div className="grid grid-cols-2 gap-4">
+                <div className="bg-gray-200 p-4 m-5">Tsunami</div>
+                <div className="bg-gray-200 p-4 m-5">Stella</div>
             </div>
-            </>
+
+        </>
     )
 }
 
