905fe0b58267d7bf394a12c2e03153423af4b2b8
bovdaboat <ignatgabov@gmail.com>
2023-06-12 22:11:44 -0700

    tailwind config

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,14 @@
 import Image from 'next/image'
 import Navbar from "@/app/shared/Navbar";
 import Hero from "@/app/shared/Hero";
-
+import Pugs from "@/app/shared/pugs";
 export default function Home() {
   return (
       <>
       <section>
           <Navbar />
             <Hero />
+          <Pugs />
           <div>Pic of the two pugs</div>
       </section>
       </>
