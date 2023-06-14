35a1e7a4e3e59bde754cac752316cc25c37a9f5f
bovdaboat <ignatgabov@gmail.com>
2023-06-13 22:03:11 -0700

    getting the Footer.tsx

--- src/app/shared/Footer.tsx
+++ src/app/shared/Footer.tsx
@@ -4,7 +4,7 @@ import React from 'react'
 const Footer = () => {
     return (
         <>
-            <div className="flex justify-end text-xl">
+            <div className="flex justify-end text-xl m-5 p5">
     <h1>Follow us on our Instagram @stell_tsunami</h1>
             </div>
             </>
